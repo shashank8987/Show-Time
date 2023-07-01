17c3e10df2551563e96bb5b6f51b21e8f524b8f4
Shashank Shekhar <84279159+shashank8987@users.noreply.github.com>
2023-07-02 00:01:26 +0530

    created server

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-routes";
+import adminRouter from "./routes/admin-routes";
+import movieRouter from "./routes/movie-routes";
+import bookingsRouter from "./routes/booking-routes";
+import cors from "cors";
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+mongoose
+  .connect(
+    `mongodb+srv://shekhar824947:QAZplm1290@cluster0.x15jzle.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connected To Database And Server is running")
+    )
+  )
+  .catch((e) => console.log(e));
